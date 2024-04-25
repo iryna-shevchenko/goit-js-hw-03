3de534d221f7c40293232938b141f1188ef14bf0
iryna-shevchenko <iryna.shevchenko.fullstack@gmail.com>
2024-04-25 10:22:03 +0300

    Summary

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,21 @@
 'use strict';
 
-// Задача 3. Ширина елемента
+// Задача 3. Фільтрація масиву чисел
 
-function getElementWidth(content, padding, border) {
-  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+function filterArray(numbers, value) {
+  let numArray = [];
+
+  for (const iterElement of numbers) {
+    if (iterElement > value) {
+      numArray.push(iterElement);
+    }
+  }
+
+  return numArray;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
